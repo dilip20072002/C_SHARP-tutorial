092441f80af29f89937ae4796bc715dd458814e5
Dilip Patel <95010587+dilip20072002@users.noreply.github.com>
2025-07-15 09:20:36 +0000

    Basic program is done

---
+++ MyApp/A_Basic.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace MyApp
+{
+    class A_Basic
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello from new repo!");
+            B_DataType.ShowData();            // C# basically run at Visual Studio platform, but I am using it in GitHub Cdoespace so every new created file will be called in First Program through Main() method, after this process we can run that new file, just like it...
+        }
+    }
+}
