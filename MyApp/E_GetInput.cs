a6902d5f1f9e5809028c3abe81f9b8a2743a21b8
Dilip Patel <95010587+dilip20072002@users.noreply.github.com>
2025-07-15 16:27:48 +0000

    Get input from user and build a calculator

--- MyApp/E_GetInput.cs
+++ MyApp/E_GetInput.cs
@@ -16,6 +16,39 @@ namespace MyApp
             Console.WriteLine("Please enter your age:");
             string age = Console.ReadLine();
             Console.WriteLine("My name is " + name + " and I am " + age + " years old.");
+
+            // Build a calculator - 
+            Console.WriteLine("Enter a number");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Choose an operation: +, -, *, /");
+            string operation = Console.ReadLine();
+            Console.WriteLine("Enter another number");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            double result = 0;
+
+            switch (operation)
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    if (num2 != 0)
+                        result = num1 / num2;
+                    else
+                        Console.WriteLine("Cannot divide by zero.");
+                    break;
+                default:
+                    Console.WriteLine("Invalid operation.");
+                    return;
+            }
+            // Display the result
+            Console.WriteLine($"The result of {num1} {operation} {num2} is: {result}");
         }
     }
 }
