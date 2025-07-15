ccc30b3b316d94bb7fc5aab3c020202115075979
Dilip Patel <95010587+dilip20072002@users.noreply.github.com>
2025-07-15 15:54:06 +0000

    Data type

--- MyApp/A_Basic.cs
+++ MyApp/A_Basic.cs
@@ -10,8 +10,10 @@ namespace MyApp
         static void Main(string[] args)
         {
             // Console.WriteLine("Hello from new repo!");
-            // B_DataType.ShowData();            // C# basically run at Visual Studio platform, but I am using it in GitHub Cdoespace so every new created file(public method) will be called in First Program through Main() method, after this process we can run that new file, just like it...
-            C_String.ShowString(args);
+            // B_DataType.ShowData(args);            // C# basically run at Visual Studio platform, but I am using it in GitHub Cdoespace so every new created file(public method) will be called in First Program through Main() method, after this process we can run that new file, just like it...
+            // C_String.ShowString(args);
+            // D_Number.ShowNumber(args);
+            // E_GetInput.GetInput(args);
         }
     }
 }
